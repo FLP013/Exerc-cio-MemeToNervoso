e1638f58cce261b6a6495b1bf1187a006ac9b7c9
FLP013 <felliperibeiro9@gmail.com>
2024-10-14 12:18:47 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string Usuario, Meme;
+
+Console.Clear();
+
+Console.WriteLine("--- MEME TO NERVOSO ---");
+
+Console.Write("Usuário, o que você pensa sobre mim? ");
+Usuario = Console.ReadLine()!;
+
+Meme = Usuario;
+
+Console.WriteLine($"- E sabe o que eu penso de você?\n {Usuario.Remove(10)}... NÃO,PERA\n Tô nervoso");
